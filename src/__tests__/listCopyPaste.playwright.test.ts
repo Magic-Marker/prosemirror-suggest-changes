e46deaaaf5741dbf93a15ee6fbf1e7343d9703a2
optimistiks <mail@potapovmax.com>
2026-07-15 18:38:20 +0400

    Add a failing test for the first list item becoming just paragraph on paste

--- src/__tests__/listCopyPaste.playwright.test.ts
+++ src/__tests__/listCopyPaste.playwright.test.ts
@@ -3,8 +3,188 @@ import { test, expect } from "./playwrightBaseTest.js";
 import { setupDocFromJSON } from "./playwrightHelpers.js";
 import { EditorPage } from "./playwrightPage.js";
 
-test.describe("Copy paste in lists", () => {
-  test("Copies a list that was inserted in track changes mode", async ({
+test.describe("copy paste in lists", () => {
+  test("with track changes off, copies a list that was added with track changes off, without the above linebreak", async ({
+    page,
+    deletionMarksVisibility,
+  }) => {
+    await setupDocFromJSON(page, {
+      type: "doc",
+      content: [
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 1" }],
+        },
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 2" }],
+        },
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 3" }],
+        },
+      ],
+    });
+
+    const editorPage = new EditorPage(page, deletionMarksVisibility);
+
+    await editorPage.focusEditor();
+    await editorPage.disableTrackChanges();
+
+    // place cursor at "Paragraph 1|"
+    await editorPage.pressKeyMultiple("ArrowUp", 2, {
+      waitForSelectionChange: true,
+    });
+
+    // input a new list
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("- ");
+    await editorPage.insertText("Item 1");
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("Item 2");
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("Item 3");
+
+    // the cursor is at "Item 3|"
+    // so now we select the whole list without the linebreak above
+    await editorPage.pressKeyMultiple("Shift+ArrowUp", 3, {
+      waitForSelectionChange: true,
+    });
+    // exclude the linebreak
+    await editorPage.pressKey("Shift+ArrowRight", {
+      waitForSelectionChange: true,
+    });
+
+    // copy selection
+    await page.keyboard.press("ControlOrMeta+c");
+
+    // reset selection to "Item 3"
+    await editorPage.pressKey("ArrowRight", { waitForSelectionChange: true });
+
+    // move cursor to "Paragraph 2|"
+    await editorPage.pressKey("ArrowDown", { waitForSelectionChange: true });
+
+    // new line
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    // because we excluded the linebreak from the selection, the first item will occupy the line we've just created
+
+    // paste
+    await editorPage.pressKey("ControlOrMeta+v", {
+      waitForSelectionChange: true,
+    });
+
+    // 3 original paragraphs + 6 paragraphs in two lists
+    await expect(editorPage.editor.locator("p")).toHaveCount(9);
+    await expect(editorPage.editor.locator("ul")).toHaveCount(2);
+    await expect(editorPage.editor.locator("li")).toHaveCount(6);
+
+    // verify the correct order of the elements (paragraph, list, paragraph, pasted list, paragraph)
+    await expect(editorPage.editor.locator("> *").nth(0)).toHaveText(
+      `Paragraph 1`,
+    );
+    await expect(editorPage.editor.locator("> *").nth(1)).toHaveText(
+      "Item 1Item 2Item 3",
+    );
+    await expect(editorPage.editor.locator("> *").nth(2)).toHaveText(
+      `Paragraph 2`,
+    );
+    await expect(editorPage.editor.locator("> *").nth(3)).toHaveText(
+      "Item 1Item 2Item 3",
+    );
+    await expect(editorPage.editor.locator("> *").nth(4)).toHaveText(
+      `Paragraph 3`,
+    );
+  });
+
+  test("with track changes off, copies a list that was added with track changes off, including the above linebreak", async ({
+    page,
+    deletionMarksVisibility,
+  }) => {
+    await setupDocFromJSON(page, {
+      type: "doc",
+      content: [
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 1" }],
+        },
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 2" }],
+        },
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 3" }],
+        },
+      ],
+    });
+
+    const editorPage = new EditorPage(page, deletionMarksVisibility);
+
+    await editorPage.focusEditor();
+    await editorPage.disableTrackChanges();
+
+    // place cursor at "Paragraph 1|"
+    await editorPage.pressKeyMultiple("ArrowUp", 2, {
+      waitForSelectionChange: true,
+    });
+
+    // input a new list
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("- ");
+    await editorPage.insertText("Item 1");
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("Item 2");
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("Item 3");
+
+    // the cursor is at "Item 3|"
+    // so now we select the whole list without the linebreak above
+    await editorPage.pressKeyMultiple("Shift+ArrowUp", 3, {
+      waitForSelectionChange: true,
+    });
+    // do NOT exclude the linebreak
+    // await editorPage.pressKey("Shift+ArrowRight", {
+    //   waitForSelectionChange: true,
+    // });
+
+    // copy selection
+    await page.keyboard.press("ControlOrMeta+c");
+
+    // reset selection to "Item 3"
+    await editorPage.pressKey("ArrowRight", { waitForSelectionChange: true });
+
+    // move cursor to "Paragraph 2|"
+    await editorPage.pressKey("ArrowDown", { waitForSelectionChange: true });
+
+    // paste
+    await editorPage.pressKey("ControlOrMeta+v", {
+      waitForSelectionChange: true,
+    });
+
+    // 3 original paragraphs + 6 paragraphs in two lists
+    await expect(editorPage.editor.locator("p")).toHaveCount(9);
+    await expect(editorPage.editor.locator("ul")).toHaveCount(2);
+    await expect(editorPage.editor.locator("li")).toHaveCount(6);
+
+    // verify the correct order of the elements (paragraph, list, paragraph, pasted list, paragraph)
+    await expect(editorPage.editor.locator("> *").nth(0)).toHaveText(
+      `Paragraph 1`,
+    );
+    await expect(editorPage.editor.locator("> *").nth(1)).toHaveText(
+      "Item 1Item 2Item 3",
+    );
+    await expect(editorPage.editor.locator("> *").nth(2)).toHaveText(
+      `Paragraph 2`,
+    );
+    await expect(editorPage.editor.locator("> *").nth(3)).toHaveText(
+      "Item 1Item 2Item 3",
+    );
+    await expect(editorPage.editor.locator("> *").nth(4)).toHaveText(
+      `Paragraph 3`,
+    );
+  });
+
+  test("with track changes on, copies and pastes a list that was added with track changes on, including the above linebreak", async ({
     page,
     deletionMarksVisibility,
   }) => {
@@ -59,6 +239,97 @@ test.describe("Copy paste in lists", () => {
 
     // move cursor to "Paragraph 2|"
     await editorPage.pressKey("ArrowDown", { waitForSelectionChange: true });
+
+    // paste
+    await editorPage.pressKey("ControlOrMeta+v", {
+      waitForSelectionChange: true,
+    });
+
+    // 3 original paragraphs + 6 paragraphs in two lists
+    await expect(editorPage.editor.locator("p")).toHaveCount(9);
+    await expect(editorPage.editor.locator("ul")).toHaveCount(2);
+    await expect(editorPage.editor.locator("li")).toHaveCount(6);
+
+    // verify the correct order of the elements (paragraph, list, paragraph, pasted list, paragraph)
+    await expect(editorPage.editor.locator("> *").nth(0)).toHaveText(
+      `Paragraph 1${PILCROW}`,
+    );
+    await expect(editorPage.editor.locator("> *").nth(1)).toHaveText(
+      "Item 1Item 2Item 3",
+    );
+    await expect(editorPage.editor.locator("> *").nth(2)).toHaveText(
+      `Paragraph 2${PILCROW}`,
+    );
+    await expect(editorPage.editor.locator("> *").nth(3)).toHaveText(
+      "Item 1Item 2Item 3",
+    );
+    await expect(editorPage.editor.locator("> *").nth(4)).toHaveText(
+      `Paragraph 3`,
+    );
+  });
+
+  test("with track changes on, copies and pastes a list that was added with track changes on, without the above linebreak", async ({
+    page,
+    deletionMarksVisibility,
+  }) => {
+    await setupDocFromJSON(page, {
+      type: "doc",
+      content: [
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 1" }],
+        },
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 2" }],
+        },
+        {
+          type: "paragraph",
+          content: [{ type: "text", text: "Paragraph 3" }],
+        },
+      ],
+    });
+
+    const editorPage = new EditorPage(page, deletionMarksVisibility);
+
+    await editorPage.focusEditor();
+    await editorPage.enableTrackChanges();
+
+    // place cursor at "Paragraph 1|"
+    await editorPage.pressKeyMultiple("ArrowUp", 2, {
+      waitForSelectionChange: true,
+    });
+
+    // input a new list
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("- ");
+    await editorPage.insertText("Item 1");
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("Item 2");
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
+    await editorPage.insertText("Item 3");
+
+    // the cursor is at "Item 3|"
+    // so now we select the whole list plus the pilcrow at the end of "Paragraph 1"
+    await editorPage.pressKeyMultiple("Shift+ArrowUp", 3, {
+      waitForSelectionChange: true,
+    });
+    // exclude the linebreak (pilcrow)
+    await editorPage.pressKey("Shift+ArrowRight", {
+      waitForSelectionChange: true,
+    });
+
+    // copy selection
+    await page.keyboard.press("ControlOrMeta+c");
+
+    // reset selection to "Item 3"
+    await editorPage.pressKey("ArrowRight", { waitForSelectionChange: true });
+
+    // move cursor to "Paragraph 2|"
+    await editorPage.pressKey("ArrowDown", { waitForSelectionChange: true });
+
+    // new line
+    await editorPage.pressKey("Enter", { waitForSelectionChange: true });
 
     // paste
     await editorPage.pressKey("ControlOrMeta+v", {
